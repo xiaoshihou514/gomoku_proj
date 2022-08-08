f81c3e5b746238cfb8c2e3119f90a358cc6ee8e3
goproxer <onelessononeexercise@gmail.com>
2022-08-08 21:50:55 +0800

    full tree impl

--- compute.py
+++ compute.py
@@ -24,7 +24,9 @@ SEARCH_DEPTH = 5
 # Computes score if a certain piece is drawn
 # $current is a 15x15 matrix with 0, 1, 2 representing none, friendly piece and enemy piece
 def compute_score_core(current, x, y):
+    # init
     final_score = 0
+    current[x][y] = 1
     # Check if it is a single piece
     x_start = min(x - 1, 0)
     x_end = max(x + 1, 14)
@@ -135,60 +137,58 @@ def compute_score_core(current, x, y):
                             break
                         piece_patterns.append(current[f_potential[1] + i][f_potential[2] + i])
                 else:
-                    exit(-1)
-                final_score += pattern.resolve_pattern_jump(piece_patterns)
-                # Checking if it blocks a three or a two
-                for e_potential in enemy_in_first_ring:
-                    # use if-else s cause I'm lazy
-                    piece_patterns = []
-                    place = e_potential[0]
-                    if place == -1:
-                        pass
-                    elif place == 0:
-                        for i in range(1, 5):
-                            # Border detection
-                            if e_potential[1] - i < 0 or e_potential[1] - i < 0:
-                                break
-                            piece_patterns.append(current[e_potential[1] - i][e_potential[2] - i])
-                    elif place <= 3:
-                        for i in range(1, 5):
-                            if e_potential[2] - i < 0:
-                                break
-                            piece_patterns.append(current[e_potential[1]][e_potential[2] - i])
-                    elif place == 4:
-                        for i in range(1, 5):
-                            if e_potential[1] + i > 14 or e_potential[2] + i > 14:
-                                break
-                            piece_patterns.append(current[e_potential[1] + i][e_potential[2] + i])
-                    elif place == 5 or place == 7 or place == 9:
-                        for i in range(1, 5):
-                            if e_potential[1] - i < 0:
-                                break
-                            piece_patterns.append(current[e_potential[1] - i][e_potential[2]])
-                    elif place == 6 or place == 8 or place == 10:
-                        for i in range(1, 5):
-                            if e_potential[1] + i < 0:
-                                break
-                            piece_patterns.append(current[e_potential[1] + i][e_potential[2]])
-                    elif place == 11:
-                        for i in range(1, 5):
-                            if e_potential[1] - i > 0 or e_potential[2] - i > 0:
-                                break
-                            piece_patterns.append(current[e_potential[1] - i][e_potential[2] - i])
-                    elif place <= 14:
-                        for i in range(1, 5):
-                            if e_potential[2] + i > 14:
-                                break
-                            piece_patterns.append(current[e_potential[1]][e_potential[2] + i])
-                    elif place == 15:
-                        for i in range(1, 5):
-                            # Border detection
-                            if e_potential[1] + i < 14 or e_potential[1] + i < 14:
-                                break
-                            piece_patterns.append(current[e_potential[1] + i][e_potential[2] + i])
-                    else:
-                        exit(-1)
-                    final_score += pattern.resolve_pattern_jump_rev(piece_patterns)
+                    final_score += pattern.resolve_pattern_jump(piece_patterns)
+                    # Checking if it blocks a three or a two
+                    for e_potential in enemy_in_first_ring:
+                        # use if-else s cause I'm lazy
+                        piece_patterns = []
+                        place = e_potential[0]
+                        if place == -1:
+                            pass
+                        elif place == 0:
+                            for i in range(1, 5):
+                                # Border detection
+                                if e_potential[1] - i < 0 or e_potential[1] - i < 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1] - i][e_potential[2] - i])
+                        elif place <= 3:
+                            for i in range(1, 5):
+                                if e_potential[2] - i < 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1]][e_potential[2] - i])
+                        elif place == 4:
+                            for i in range(1, 5):
+                                if e_potential[1] + i > 14 or e_potential[2] + i > 14:
+                                    break
+                                piece_patterns.append(current[e_potential[1] + i][e_potential[2] + i])
+                        elif place == 5 or place == 7 or place == 9:
+                            for i in range(1, 5):
+                                if e_potential[1] - i < 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1] - i][e_potential[2]])
+                        elif place == 6 or place == 8 or place == 10:
+                            for i in range(1, 5):
+                                if e_potential[1] + i < 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1] + i][e_potential[2]])
+                        elif place == 11:
+                            for i in range(1, 5):
+                                if e_potential[1] - i > 0 or e_potential[2] - i > 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1] - i][e_potential[2] - i])
+                        elif place <= 14:
+                            for i in range(1, 5):
+                                if e_potential[2] + i > 14:
+                                    break
+                                piece_patterns.append(current[e_potential[1]][e_potential[2] + i])
+                        elif place == 15:
+                            for i in range(1, 5):
+                                # Border detection
+                                if e_potential[1] + i < 14 or e_potential[1] + i < 14:
+                                    break
+                                piece_patterns.append(current[e_potential[1] + i][e_potential[2] + i])
+                        else:
+                            final_score += pattern.resolve_pattern_jump_rev(piece_patterns)
     else:
         # Case if the piece is not a single
         # Checks LT-RB U-D RT-LB L-R
@@ -225,6 +225,208 @@ def compute_score_core(current, x, y):
         final_score += pattern.resolve_pattern_cont(l_r)
         l_r[4] = 2
         final_score += pattern.resolve_pattern_cont_rev(l_r)
+
+def compute_score_rev_core(current, x, y):
+    final_score = 0
+    current[x][y] = 2
+    # Check if it is a single piece
+    x_start = min(x - 1, 0)
+    x_end = max(x + 1, 14)
+    y_start = min(y - 1, 0)
+    y_end = max(y + 1, 14)
+    is_single = True
+    first_ring = []
+    for i in range(x_start, x_end):
+        for j in range(y_start, y_end):
+            block = current[x][y]
+            if block != 0:
+                is_single = False
+                first_ring.append(([block, i, j]))
+    if is_single:
+        final_score += ONE
+        # Manually compute the first ring (5x5)
+        # If it's border a placeholder [-1] is inserted
+        if y - 2 < 0:
+            for i in range(1, 6):
+                first_ring.append([-1])
+        else:
+            for i in range(-2, 3):
+                if x + i < 0 or x + i > 14:
+                    first_ring.append([-1])
+                else:
+                    first_ring.append([current[x + i][y - 2], x + i, y - 2])
+        for i in range(-1, 2):
+            if x - 2 < 0:
+                first_ring.append([-1])
+            else:
+                first_ring.append([current[x - 2][y + i], x - 2, y + i])
+            if x + 2 > 14:
+                first_ring.append([-1])
+            else:
+                first_ring.append([current[x + 2][y + i], x + 2, y + i])
+        if y + 2 > 14:
+            for i in range(1, 6):
+                first_ring.append([-1])
+        else:
+            for i in range(-2, 3):
+                if x + i < 0 or x + i > 14:
+                    first_ring.append([-1])
+                else:
+                    first_ring.append([current[x + i][y + 2], x + i, y + 2])
+
+        # If it is a single, we would check if it blocks a potential enemy line
+        # or if makes a jump three, four or five
+        # First check for jumps:
+        friend_in_first_ring = []
+        enemy_in_first_ring = []
+        for i in range(len(first_ring)):
+            stat = first_ring[i]
+            if stat[0] == 2:
+                # The original identifier would be used tell the algorithm which direction to check
+                stat[0] = i
+                friend_in_first_ring.append(stat)
+            else:
+                # If it's not a friendly piece it must be an enemy
+                stat[0] = i
+                enemy_in_first_ring.append(stat)
+            # We give score if it makes a jump_three or jump_four, with itself being the jump
+            for f_potential in friend_in_first_ring:
+                # use if-else s cause I'm lazy
+                piece_patterns = []
+                place = f_potential[0]
+                if place == -1:
+                    pass
+                elif place == 0:
+                    for i in range(1, 5):
+                        # Border detection
+                        if f_potential[1] - i < 0 or f_potential[1] - i < 0:
+                            break
+                        piece_patterns.append(current[f_potential[1] - i][f_potential[2] - i])
+                elif place <= 3:
+                    for i in range(1, 5):
+                        if f_potential[2] - i < 0:
+                            break
+                        piece_patterns.append(current[f_potential[1]][f_potential[2] - i])
+                elif place == 4:
+                    for i in range(1, 5):
+                        if f_potential[1] + i > 14 or f_potential[2] + i > 14:
+                            break
+                        piece_patterns.append(current[f_potential[1] + i][f_potential[2] + i])
+                elif place == 5 or place == 7 or place == 9:
+                    for i in range(1, 5):
+                        if f_potential[1] - i < 0:
+                            break
+                        piece_patterns.append(current[f_potential[1] - i][f_potential[2]])
+                elif place == 6 or place == 8 or place == 10:
+                    for i in range(1, 5):
+                        if f_potential[1] + i < 0:
+                            break
+                        piece_patterns.append(current[f_potential[1] + i][f_potential[2]])
+                elif place == 11:
+                    for i in range(1, 5):
+                        if f_potential[1] - i > 0 or f_potential[2] - i > 0:
+                            break
+                        piece_patterns.append(current[f_potential[1] - i][f_potential[2] - i])
+                elif place <= 14:
+                    for i in range(1, 5):
+                        if f_potential[2] + i > 14:
+                            break
+                        piece_patterns.append(current[f_potential[1]][f_potential[2] + i])
+                elif place == 15:
+                    for i in range(1, 5):
+                        # Border detection
+                        if f_potential[1] + i < 14 or f_potential[1] + i < 14:
+                            break
+                        piece_patterns.append(current[f_potential[1] + i][f_potential[2] + i])
+                else:
+                    final_score += pattern.resolve_pattern_jump_rev(piece_patterns)
+                    # Checking if it blocks a three or a two
+                    for e_potential in enemy_in_first_ring:
+                        # use if-else s cause I'm lazy
+                        piece_patterns = []
+                        place = e_potential[0]
+                        if place == -1:
+                            pass
+                        elif place == 0:
+                            for i in range(1, 5):
+                                # Border detection
+                                if e_potential[1] - i < 0 or e_potential[1] - i < 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1] - i][e_potential[2] - i])
+                        elif place <= 3:
+                            for i in range(1, 5):
+                                if e_potential[2] - i < 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1]][e_potential[2] - i])
+                        elif place == 4:
+                            for i in range(1, 5):
+                                if e_potential[1] + i > 14 or e_potential[2] + i > 14:
+                                    break
+                                piece_patterns.append(current[e_potential[1] + i][e_potential[2] + i])
+                        elif place == 5 or place == 7 or place == 9:
+                            for i in range(1, 5):
+                                if e_potential[1] - i < 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1] - i][e_potential[2]])
+                        elif place == 6 or place == 8 or place == 10:
+                            for i in range(1, 5):
+                                if e_potential[1] + i < 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1] + i][e_potential[2]])
+                        elif place == 11:
+                            for i in range(1, 5):
+                                if e_potential[1] - i > 0 or e_potential[2] - i > 0:
+                                    break
+                                piece_patterns.append(current[e_potential[1] - i][e_potential[2] - i])
+                        elif place <= 14:
+                            for i in range(1, 5):
+                                if e_potential[2] + i > 14:
+                                    break
+                                piece_patterns.append(current[e_potential[1]][e_potential[2] + i])
+                        elif place == 15:
+                            for i in range(1, 5):
+                                # Border detection
+                                if e_potential[1] + i < 14 or e_potential[1] + i < 14:
+                                    break
+                                piece_patterns.append(current[e_potential[1] + i][e_potential[2] + i])
+                        else:
+                            final_score += pattern.resolve_pattern_jump_rev(piece_patterns)
+    else:
+        # Case if the piece is not a single
+        # Checks LT-RB U-D RT-LB L-R
+        lt_rb = []
+        u_d = []
+        rt_lb = []
+        l_r = []
+        for i in range(-4, 5):
+            if 0 <= y + i <= 14:
+                l_r.append(current[x][y + i])
+            else:
+                l_r.append(-1)
+            if 0 <= x + i <= 14:
+                u_d.append(current[x + i][y])
+            else:
+                u_d.append(-1)
+            if 0 <= y + i <= 14 and 0 <= x + i <= 14:
+                rt_lb.append(current[x + i][y + i])
+            else:
+                rt_lb.append(-1)
+            if 0 <= y + i <= 14 and 0 <= x - i <= 14:
+                lt_rb.append(current[x - i][y + i])
+            else:
+                lt_rb.append(-1)
+        final_score+=pattern.resolve_pattern_cont_rev(lt_rb)
+        lt_rb[4] = 1
+        final_score+=pattern.resolve_pattern_cont(lt_rb)
+        final_score+=pattern.resolve_pattern_cont_rev(u_d)
+        u_d[4] = 1
+        final_score+=pattern.resolve_pattern_cont(u_d)
+        final_score += pattern.resolve_pattern_cont_rev(rt_lb)
+        rt_lb[4] = 1
+        final_score+= pattern.resolve_pattern_cont(rt_lb)
+        final_score += pattern.resolve_pattern_cont_rev(l_r)
+        l_r[4] = 1
+        final_score += pattern.resolve_pattern_cont(l_r)
 
 def compute_score(current, x, y, cache):
     cache.append([x, y, compute_score_core(current, x, y)])
--- search.py
+++ search.py
@@ -1,42 +1,111 @@
+from ast import Param
 from threading import Thread
 import compute
 
+# I think depth can only be odd
+# If it's the last search, it would return an array [score, x, y]
+# Else, it would trace recursively to find the best move and return [score, x , y]
+# Parent score is used to prune nodes
 def search(depth, current):
-        tree = []
-        for i in range(0,15):
-            for j in range(0,15):
-                is_upper_bound = j == 0
-                is_lower_bound = j == 14
-                is_left_bound = i == 0
-                is_right_bound = i == 14
-                # Check if there's a non empty neighbor and compute score if so
-                if not is_upper_bound:
-                    if current[i][j-1] != 0:    
+    tree = []
+    for i in range(0,15):
+        for j in range(0,15):
+            is_upper_bound = j == 0
+            is_lower_bound = j == 14
+            is_left_bound = i == 0
+            is_right_bound = i == 14
+            # Check if there's a non empty neighbor and compute score if so
+            if not is_upper_bound:
+                if current[i][j-1] != 0:    
+                    thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
+                    thread.start()
+                    thread.join()
+                    pass
+                if not is_left_bound:
+                    if current[i-1][j-1] != 0 or current[i-1][j] != 0: 
                         thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
                         thread.start()
                         thread.join()
                         pass
-                    if not is_left_bound:
-                        if current[i-1][j-1] != 0 or current[i-1][j] != 0: 
-                            thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
+                if not is_right_bound:
+                    if current[i+1][j-1] or current[i+1][j] != 0: 
+                        thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
+                        thread.start()
+                        thread.join()    
+            if not is_lower_bound:
+                if current[i][j+1] != 0:
+                    thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
+                    thread.start()
+                    thread.join()
+                if not is_left_bound and current[i-1][j+1]:
+                    thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
+                    thread.start()
+                    thread.join()
+                if not is_right_bound and current[i+1][j+1]:
+                    thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
+                    thread.start()
+                    thread.join()     
+    if depth == 1:
+        max = tree[0]
+        for item in range(0,len(tree)):
+            if item[0] > max[0]:
+                max = item
+        return max        
+    else:
+        score_board = []
+        for item in tree:
+            context = commit_move(item[0], item[2], current)
+            enemy_tree = []
+            for i in range(0,15):
+                for j in range(0,15):
+                    is_upper_bound = j == 0
+                    is_lower_bound = j == 14
+                    is_left_bound = i == 0
+                    is_right_bound = i == 14
+                    # Check if there's a non empty neighbor and compute score if so
+                    if not is_upper_bound:
+                        if current[i][j-1] != 0:    
+                            thread = Thread(target=compute.compute_score_rev_core,args=(context,i,j,enemy_tree))
                             thread.start()
                             thread.join()
                             pass
-                    if not is_right_bound:
-                        if current[i+1][j-1] or current[i+1][j] != 0: 
-                            thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
+                        if not is_left_bound:
+                            if current[i-1][j-1] != 0 or current[i-1][j] != 0: 
+                                thread = Thread(target=compute.compute_score_rev_core,args=(context,i,j,enemy_tree))
+                                thread.start()
+                                thread.join()
+                                pass
+                        if not is_right_bound:
+                            if current[i+1][j-1] or current[i+1][j] != 0: 
+                                thread = Thread(target=compute.compute_score_rev_core,args=(context,i,j,enemy_tree))
+                                thread.start()
+                                thread.join()    
+                    if not is_lower_bound:
+                        if current[i][j+1] != 0:
+                            thread = Thread(target=compute.compute_score_rev_core,args=(context,i,j,enemy_tree))
                             thread.start()
-                            thread.join()    
-                        if not is_lower_bound:
-                            if current[i][j+1] != 0:
-                                thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
-                                thread.start()
-                                thread.join()
-                            if not is_left_bound and current[i-1][j+1]:
-                                thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
-                                thread.start()
-                                thread.join()
-                            if not is_right_bound and current[i+1][j+1]:
-                                thread = Thread(target=compute.compute_score,args=(current,i,j,tree))
-                                thread.start()
-                                thread.join()     
+                            thread.join()
+                        if not is_left_bound and current[i-1][j+1]:
+                            thread = Thread(target=compute.compute_score_rev_core,args=(context,i,j,enemy_tree))
+                            thread.start()
+                            thread.join()
+                        if not is_right_bound and current[i+1][j+1]:
+                            thread = Thread(target=compute.compute_score_rev_core,args=(context,i,j,enemy_tree))
+                            thread.start()
+                            thread.join() 
+            max = enemy_tree[0]
+            for item in enemy_tree:
+                if item[0] > max[0]:
+                    max = item   
+            new_depth = depth - 2
+            context = commit_move(max[1], max[2], context)
+            next_best_move = search(new_depth, context)         
+            score_board.append(item[1],item[2],item[0]-max[0]+next_best_move[0]) 
+        for item in score_board:
+            if item[0] > max[0]:
+                max = item   
+        return max
+             
+def commit_move(x, y, current):
+    current[x][y] = 1
+    return current                    
