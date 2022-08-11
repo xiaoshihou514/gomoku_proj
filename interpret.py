bb9fa59f22da420df52d45b20cdff750aa2dafe5
Goproxer <108414369+Goproxer@users.noreply.github.com>
2022-08-11 21:24:26 +0800

    added a method to see if someone has won

--- interpret.py
+++ interpret.py
@@ -1,26 +1,66 @@
 # $current is a 15x15 matrix
 # returns a list of bools [someone_has_won,is_black_winning]
-from nis import match
+
+import re
 
 
-def has_win_state(current):
+WIN_CONDITION = [1,1,1,1,1]
+ENEMY_WIN_CONDITION = [2,2,2,2,2]
+
+def is_at_win_state(current):
     # horizontal detection
     for i in range(0,15):
         for j in range(0,10):
-            if current[i][j:j+5] == [1,1,1,1,1]:
+            if current[i][j:j+5] == WIN_CONDITION:
                 return [True,True]
-            elif current[i][j:j+5] == [2,2,2,2,2]:
+            elif current[i][j:j+5] == ENEMY_WIN_CONDITION:
                 return [True,False]
     # vertical detection
     for i in range(0,10):
         for j in range(0,15):
-            if current[i:i+5][j] == [1,1,1,1,1]:
+            if current[i:i+5][j] == WIN_CONDITION:
                 return [True,True]
-            elif current[i:i+5][j] == [2,2,2,2,2]:
+            elif current[i:i+5][j] == ENEMY_WIN_CONDITION:
                 return [True,False]
-    # diagnal detection
+    # diagonal detection
     for i in range(4,11):
-        match = []
+        match =[]
         for x in range(0,i+1):
-            # !TODO
-            pass 
+            match.append(current[i-x][i+x])
+        for j in range(0,len(match)-5):
+            slice = match[j:j+5]
+            if slice == WIN_CONDITION:
+                return [True,True]
+            elif slice == ENEMY_WIN_CONDITION:
+                return [True,False]
+    for i in range(10,3):
+        match = []
+        for x in range(0,14-i):
+            match.append(current[i-x][i+x])
+        for j in range(0,len(match)-5):
+            slice = match[j:j+5]
+            if slice == WIN_CONDITION:
+                return [True,True]
+            elif slice == ENEMY_WIN_CONDITION:
+                return [True,False]
+    for i in range(10,3):
+        match = []
+        for x in range(0,14-i):
+            match.append(current[i+x][i+x])
+        for j in range(0,len(match)-5):
+            slice = match[j:j+5]
+            if slice == WIN_CONDITION:
+                return [True,True]
+            elif slice == ENEMY_WIN_CONDITION:
+                return [True,False]
+    for i in range(4,11):
+        match =[]
+        for x in range(0,i+1):
+            match.append(current[i-x][i-x])
+        for j in range(0,len(match)-5):
+            slice = match[j:j+5]
+            if slice == WIN_CONDITION:
+                return [True,True]
+            elif slice == ENEMY_WIN_CONDITION:
+                return [True,False]
+    return [False,False]
