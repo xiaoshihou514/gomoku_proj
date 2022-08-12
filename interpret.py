f47cb1fe119290c1389b159e8c2c1517d74e4581
Goproxer <108414369+Goproxer@users.noreply.github.com>
2022-08-12 22:10:47 +0800

    interpret.py is now bug free

--- interpret.py
+++ interpret.py
@@ -1,13 +1,14 @@
 # $current is a 15x15 matrix
 # returns a list of bools [someone_has_won,is_black_winning]
 
-import re
-
-
 WIN_CONDITION = [1,1,1,1,1]
 ENEMY_WIN_CONDITION = [2,2,2,2,2]
 
 def is_at_win_state(current):
+    print()
+    for some in current:
+        print("",some)
+    print()
     # horizontal detection
     for i in range(0,15):
         for j in range(0,10):
@@ -16,51 +17,63 @@ def is_at_win_state(current):
             elif current[i][j:j+5] == ENEMY_WIN_CONDITION:
                 return [True,False]
     # vertical detection
-    for i in range(0,10):
+    for i in range(0,15):
+        col = []
         for j in range(0,15):
-            if current[i:i+5][j] == WIN_CONDITION:
+            col.append(current[j][i])
+        for k in range(0,10):
+            if col[k:k+5] == WIN_CONDITION:
                 return [True,True]
-            elif current[i:i+5][j] == ENEMY_WIN_CONDITION:
-                return [True,False]
+            elif col[k:k+5] == ENEMY_WIN_CONDITION:
+                return [True, False]    
     # diagonal detection
-    for i in range(4,11):
+    for i in range(4,15):
         match =[]
         for x in range(0,i+1):
-            match.append(current[i-x][i+x])
-        for j in range(0,len(match)-5):
+            match.append(current[i-x][x])
+        for j in range(0,len(match)-4):
             slice = match[j:j+5]
             if slice == WIN_CONDITION:
                 return [True,True]
             elif slice == ENEMY_WIN_CONDITION:
                 return [True,False]
-    for i in range(10,3):
+    for i in range(4,15):
         match = []
-        for x in range(0,14-i):
-            match.append(current[i-x][i+x])
-        for j in range(0,len(match)-5):
+        for x in range(0,i+1):
+            match.append(current[14-i-x][14-i+x])
+        for j in range(0,len(match)-4):
             slice = match[j:j+5]
             if slice == WIN_CONDITION:
                 return [True,True]
             elif slice == ENEMY_WIN_CONDITION:
                 return [True,False]
-    for i in range(10,3):
-        match = []
-        for x in range(0,14-i):
-            match.append(current[i+x][i+x])
-        for j in range(0,len(match)-5):
-            slice = match[j:j+5]
-            if slice == WIN_CONDITION:
-                return [True,True]
-            elif slice == ENEMY_WIN_CONDITION:
-                return [True,False]
-    for i in range(4,11):
+    # diagonal detection rotated
+    for i in range(4,15):
         match =[]
         for x in range(0,i+1):
-            match.append(current[i-x][i-x])
-        for j in range(0,len(match)-5):
+            match.append(current[x][14-i+x])
+        for j in range(0,len(match)-4):
+            slice = match[j:j+5]
+            if slice == WIN_CONDITION:
+                return [True,True]
+            elif slice == ENEMY_WIN_CONDITION:
+                return [True,False]
+    for i in range(4,15):
+        match =[]
+        for x in range(0,i+1):
+            match.append(current[14-i+x][x])
+        for j in range(0,len(match)-4):
             slice = match[j:j+5]
             if slice == WIN_CONDITION:
                 return [True,True]
             elif slice == ENEMY_WIN_CONDITION:
                 return [True,False]
     return [False,False]
+
+# Check if the board has sth other than 0, 1 or 2
+def board_corrupted(board):
+    for row in board:
+        for item in row:
+            if item != 0 and item != 1 and item != 2:
+                return True
+    return False
