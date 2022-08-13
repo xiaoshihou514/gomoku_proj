2e5d22822abb273190efa3a3e72f3de959d1dbbe
JasonSihan <jasonchencorrection@gmail.com>
2022-08-13 05:09:10 -0500

    Draw pieces and show turns change

--- chessboard.py
+++ chessboard.py
@@ -1,15 +1,14 @@
-from imp import get_tag
 import tkinter as tk
 from tkinter import *
 
 PIECE_SIZE = 10
 piece_color = "black"
 
+'''Store the location that the pieces have been put'''
 axis_black = []
 axis_white = []
 
 person_flag = 1
-piece_color = "black"
 color = "black"
 
 
@@ -18,7 +17,7 @@ root_window = tk.Tk()
 root_window.title("Gomoku Game by Kevin Ye & Sihan Chen")
 root_window.geometry("995x660")
 root_window.resizable(0,0)
-#TODO Please change the path of ico after you clone the repo
+#TODO Please change the path of ico to your path after you clone the repo
 root_window.iconbitmap('./GomokuIcon.ico')
 
 
@@ -27,7 +26,7 @@ frames = tk.Canvas(root_window, width=220, height=70)
 frames.grid(row = 0, column = 1)
 frames.create_oval(110 - PIECE_SIZE, 25 - PIECE_SIZE, 110 + PIECE_SIZE, 25 + PIECE_SIZE, fill = piece_color, tags = ("show_pieces"))
 var = tk.StringVar()
-var.set("Holding Black")
+var.set("Your turn")
 person_chess = tk.Label(root_window, textvariable = var, width = 20, anchor = tk.CENTER, font = ("Times New Roman", 15) )
 person_chess.place(x = 650, y = 100)
 ''''''
@@ -41,7 +40,9 @@ board = tk.Canvas(root_window, bg = "#F5DEB3", width = 640, height = 640)
 board.grid(row = 0, column = 0, rowspan = 6)
 #line
 for i in range(15):
+    # Vertical Line
     board.create_line(32, (42 * i + 38), 622, (42 * i + 38))
+    # Horizontal Line
     board.create_line((42 * i + 32), 38, (42 * i + 32), 628)
 #point
 point_x = [3, 3, 11, 11, 7]
@@ -58,6 +59,17 @@ for i in range(65, 81):
     label = tk.Label(board, text = chr(i), fg = "black", bg = "#F5DEB3")
     label.place(x = 42 * count + 25, y = 2)
     count += 1
+
+'''Set of intersection of lines'''
+'''Each element is a 2-element array'''
+intersection = []
+for i in range(15):
+    for j in range(15):
+        intersection.append([(42 * i + 32),(42 * j + 38)])
+    
+# print(intersection[0][0])
+
+
 
 
 #TODO Fix the function of tk
@@ -96,29 +108,44 @@ click_y = 0
 def click_cor(event):
     global click_x, click_y
     print(f"Left Click cooridinate:x= {event.x} y= {event.y}")
-    click_x = {event.x}
-    click_y = {event.y}
+    click_x = event.x
+    click_y = event.y
     ChessPos()        
 
 '''Bind the mouse click with chess board'''
 board.bind("<Button-1>", click_cor)
 board.bind("<Double-Button-1>", click_cor)
 
-'''Find the closest intersection of lines and put the chess down'''
+'''Find the closest intersection of lines'''
 def ChessPos():
-    global click_x, click_y, person_chess, person_flag
-    # intersect = board.find_closest(click_x, click_y)
-    # piece_tuple = board.gettags(intersect)
+    global click_x, click_y, person_chess, person_flag, pieces_x, pieces_y, intersection, piece_color
+    putPiece(piece_color)
+    #putPiece(piece_color)
 
 
 '''Draw piece after find the chess position'''
 def putPiece(piece_color):
-    global axis_black, axis_white
+    global axis_black, axis_white, click_x, click_y
     board.create_oval(click_x - PIECE_SIZE, click_y - PIECE_SIZE, click_x + PIECE_SIZE, click_y + PIECE_SIZE, fill = piece_color, tags = ("piece"))
     if piece_color == "white":
-        axis_white.append((click_x, click_y))
+        axis_white.append([click_x, click_y])
+        showChange("black")
     elif piece_color == "black":
-        axis_black.append((click_x, click_y))
+        axis_black.append([click_x, click_y])
+        showChange("white")
     #TODO Add the judge of game position: win to end/continues?  
+
+def showChange(color):
+    global piece_color, var, person_chess, frames
+    piece_color = color
+    frames.delete("show_piece")
+    frames.create_oval(110 - PIECE_SIZE, 25 - PIECE_SIZE, 110 + PIECE_SIZE, 25 + PIECE_SIZE, fill = piece_color, tags = ("show_piece"))
+    if color == "black":
+        var.set("Your turn")
+    elif color == "white":
+        var.set("AI's turn")
+    person_chess.destroy()
+    person_chess = tk.Label(root_window, textvariable = var, width = 20, anchor = tk.CENTER, font = ("Times New Roman", 15) )
+    person_chess.place(x = 650, y = 100)
 
 root_window.mainloop()
