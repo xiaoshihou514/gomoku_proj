b83cbfe2f95f35e3224492a79b49df4599e1198a
Goproxer <108414369+Goproxer@users.noreply.github.com>
2022-08-14 08:14:32 +0800

    Add files via upload

--- chessboard.py
+++ chessboard.py
@@ -1,23 +1,40 @@
 import tkinter as tk
 from tkinter import *
+import interpret
+import search
+import math
 
 PIECE_SIZE = 10
 piece_color = "black"
+win_color = "black"
+'''Store the location that the pieces have been put, 15*15 matrix'''
+# TODO: store_chess = [[0]*15]*15: initiation in this way will let store_chess[x][x] = 1 change x cols in all rows be 1
+store_chess = [[0 for i in range(15)] for j in range(15)]
+# store_chess = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+#            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
-axis_black = []
-axis_white = []
-
-person_flag = 1
-piece_color = "black"
-color = "black"
-
+'''marker of winners: 1 as black wins, 2 as white wins, 0 as none of player wins'''
+person_flag = 0
 
 #root window as chess board
 root_window = tk.Tk()
 root_window.title("Gomoku Game by Kevin Ye & Sihan Chen")
 root_window.geometry("995x660")
 root_window.resizable(0,0)
-root_window.iconbitmap('./GomokuIcon.ico')
+root_window.iconbitmap('./GomokuIcon.ico') #Icon is from https://www.aigei.com/s?q=%E6%A3%8B&type=icon_7
 
 
 #chess info
@@ -25,11 +42,9 @@ frames = tk.Canvas(root_window, width=220, height=70)
 frames.grid(row = 0, column = 1)
 frames.create_oval(110 - PIECE_SIZE, 25 - PIECE_SIZE, 110 + PIECE_SIZE, 25 + PIECE_SIZE, fill = piece_color, tags = ("show_pieces"))
 var = tk.StringVar()
-var.set("Holding Black")
+var.set("Your turn")
 person_chess = tk.Label(root_window, textvariable = var, width = 20, anchor = tk.CENTER, font = ("Times New Roman", 15) )
-person_chess.grid(row = 1, column = 1)
-pieces_x = [i for i in range(32, 623, 42)]
-pieces_y = [i for i in range(38, 629, 42)]
+person_chess.place(x = 650, y = 100)
 
 '''Print the chess board'''
 #background
@@ -38,7 +53,9 @@ board = tk.Canvas(root_window, bg = "#F5DEB3", width = 640, height = 640)
 board.grid(row = 0, column = 0, rowspan = 6)
 #line
 for i in range(15):
+    # Vertical Line
     board.create_line(32, (42 * i + 38), 622, (42 * i + 38))
+    # Horizontal Line
     board.create_line((42 * i + 32), 38, (42 * i + 32), 628)
 #point
 point_x = [3, 3, 11, 11, 7]
@@ -56,29 +73,142 @@ for i in range(65, 81):
     label.place(x = 42 * count + 25, y = 2)
     count += 1
 
-# banner = tk.Label(root_window, text='Choose Chess Color', font=('Times New Roman', 13), bg = "#F5DEB3").place(x=50, y=70, anchor='nw')
-# on_hit = False
-# CheckVar = tk.IntVar()
-# black_choice = tk.Radiobutton(root_window, text="black", variable=CheckVar, value=1, anchor = 'w').place(x = 50, y = 100)
-# white_choice = tk.Radiobutton(root_window, text="white", variable=CheckVar, value=2, anchor = 'w').place(x = 50, y= 130)
-# if CheckVar.get() == "black" or CheckVar.get() == "white":
-#     on_hit = True
-#     color = CheckVar.get()
-#     message = tkinter.messagebox.askokcancel(title = 'color', message = color)
-# def getColor():
-#     global color, on_hit, black_choice, white_choice
-#     if on_hit:
-#         ChessKind(color)
+'''Set of intersection of lines'''
+'''Each element is a 2-element array'''
+intersection = []
+for i in range(15):
+    for j in range(15):
+        intersection.append([(42 * i + 32),(42 * j + 38)])
+#store intersects(x,y) in x first order: scan all intersects in one x then moves to x_1=x+42    
+# print(intersection)
+#Import search(AI Steps) and interpret(game position) (1 - our piece) (2 - enemy)
 
-def ChessKind(color):
-    global piece_color
+'''
+Mouse Click Event
+<Button-1>  Left
+<Button-2>  Right
+<Double-Button-1>   Double Left Click
+<Double-Button-2>   Double Right Click
+'''
+click_x = 0
+click_y = 0
+'''Mouse click range'''
+pieces_x = [i for i in range(32, 623, 42)]
+pieces_y = [i for i in range(38, 629, 42)]
+
+'''Return the coordinate of click'''
+def click_cor(event):
+    global click_x, click_y
+    print(f"Left Click cooridinate:x= {event.x} y= {event.y}")
+    click_x = event.x
+    click_y = event.y
+    ChessPos()
+
+'''Bind the mouse click with chess board'''
+board.bind("<Button-1>", click_cor)
+#board.bind("<Double-Button-1>", click_cor)
+
+'''Find the closest intersection of lines'''
+def ChessPos():
+    global click_x, click_y, person_flag, pieces_x, pieces_y, intersection, piece_color, store_chess
+    min_dist = 255
+    chess_x = -1
+    chess_y = -1
+    for i in range(len(intersection)):
+        delta_x = math.pow((click_x - intersection[i][0]), 2)
+        delta_y = math.pow((click_y - intersection[i][1]), 2)
+        dist = math.sqrt(delta_x + delta_y)
+        if dist < min_dist:
+            min_dist = dist
+            chess_x = intersection[i][0]
+            chess_y = intersection[i][1]
+    check = search.search(3, store_chess)
+    #TODO
+    '''Try depth = 3 in search.py, then less/more depths'''
+    # print("", check)
+    # print(f"chess_x = {chess_x}, chess_y = {chess_y}")
+    putPiece(piece_color, chess_x, chess_y)
+
+
+'''Draw piece after find the chess position'''
+def putPiece(piece_color, chess_x, chess_y):
+    global store_chess, person_flag, board
+    board.create_oval(chess_x - PIECE_SIZE, chess_y - PIECE_SIZE, chess_x + PIECE_SIZE, chess_y + PIECE_SIZE, fill = piece_color, tags = ("piece"))
+    cols_float = (chess_x - 32)/42
+    rows_float = (chess_y - 38)/42
+    # print(f"rows_float = {rows_float}, cols_float = {cols_float}")
+    rows = math.ceil(rows_float)
+    cols = math.ceil(cols_float)
+    print(f"rows = {rows}, cols = {cols}")
+    if store_chess[rows][cols] == 0:
+        if piece_color == "white":
+            store_chess[rows][cols] = 2
+            ChangeTurn("black")
+            print("white")
+        elif piece_color == "black":
+            store_chess[rows][cols] = 1
+            ChangeTurn("white")
+            print("black")
+        print()
+        for some in store_chess:
+            print("",some)
+        print()        
+    #print(store_chess) #check correction of position
+    #TODO Add the judge of game position: win to end/continues?
+    # Check Game Position each turn
+    result = interpret.is_at_win_state(store_chess)
+    if result[0] == True:
+        end_message.set("Game Ends")
+        if result[1] == True:
+            person_flag = 1
+            win_message.set("You Win!")
+        elif result[1] == False:
+            person_flag = 2
+            win_message.set("AI Wins!")
+        board.unbind("<Button-1>", "")    
+
+
+def ChangeTurn(color):
+    global piece_color, var, person_chess, frames
     piece_color = color
     frames.delete("show_piece")
     frames.create_oval(110 - PIECE_SIZE, 25 - PIECE_SIZE, 110 + PIECE_SIZE, 25 + PIECE_SIZE, fill = piece_color, tags = ("show_piece"))
+    if color == "black":
+        var.set("Your turn")
+    elif color == "white":
+        var.set("AI's turn")
+    person_chess.destroy()
+    person_chess = tk.Label(root_window, textvariable = var, width = 20, anchor = tk.CENTER, font = ("Times New Roman", 15) )
+    person_chess.place(x = 650, y = 100)
 
+'''Define reset button'''
+def gameReset():
+    global store_chess, piece_color, person_flag, intersection, board
+    var.set("Holding Black")      
+    win_message.set("")
+    end_message.set("")         
+    ChangeTurn("black")             
+    board.delete("piece")
+    store_chess = [[0 for i in range(15)] for j in range(15)]
+    piece_color = "black"
+    person_flag = 0
+    board.bind("<Button-1>", click_cor)
 
+'''Label of Who is the winner'''
+win_message = tk.StringVar()
+win_message.set("")
+result_label = tk.Label(root_window, textvariable = win_message, width = 12, height = 4, anchor = tk.CENTER, fg = "red", font = ("Arial", 25) )
+result_label.grid(row = 2, column = 1, rowspan = 2)
 
-        
+'''Labal of Game Ends'''
+end_message = tk.StringVar()
+end_message.set("")
+game_label = tk.Label(root_window, textvariable = end_message, width = 12, height = 4, anchor = tk.CENTER, font = ("Arial", 18) )
+game_label.grid(row = 4, column = 1)
+
+'''Restart Button'''
+reset_button = tk.Button(root_window, text = "Restart", font = ("Arial", 20), width = 8, command = gameReset)
+reset_button.grid(row = 5, column = 1)
 
 
 root_window.mainloop()
---
+++ search_opening.py
@@ -0,0 +1,145 @@
+from threading import Thread
+import time
+import compute_opening
+
+# I think depth can only be odd
+# If it's the last search, it would return an array [score, x, y]
+# Else, it would trace recursively to find the best move and return [score, x , y]
+# Parent score is used to prune nodes
+def search(depth, current):
+    tree = []
+    count = 0
+    for i in range(0,15):
+        for j in range(0,15):
+            if current[i][j] != 0:
+                continue
+            is_upper_bound = j == 0
+            is_lower_bound = j == 14
+            is_left_bound = i == 0
+            is_right_bound = i == 14
+            # Check if there's a non empty neighbor and compute score if so
+            if not is_upper_bound:
+                if current[i][j-1] != 0:    
+                    # multi threading
+                    thread = Thread(target=compute_opening.compute_score,args=(current.copy(),i,j,tree))
+                    thread.start()
+                    count += 1
+                if not is_left_bound:
+                    if current[i-1][j-1] != 0 or current[i-1][j] != 0: 
+                        thread = Thread(target=compute_opening.compute_score,args=(current.copy(),i,j,tree))
+                        thread.start()
+                        count += 1
+                if not is_right_bound:
+                    if current[i+1][j-1] or current[i+1][j] != 0: 
+                        thread = Thread(target=compute_opening.compute_score,args=(current.copy(),i,j,tree))
+                        thread.start()
+                        count += 1
+            if not is_lower_bound:
+                if current[i][j+1] != 0:
+                    thread = Thread(target=compute_opening.compute_score,args=(current.copy(),i,j,tree))
+                    thread.start()
+                    count += 1
+                if not is_left_bound and current[i-1][j+1]:
+                    thread = Thread(target=compute_opening.compute_score,args=(current.copy(),i,j,tree))
+                    thread.start()
+                    count += 1
+                if not is_right_bound and current[i+1][j+1]:
+                    thread = Thread(target=compute_opening.compute_score,args=(current.copy(),i,j,tree))
+                    thread.start()
+                    count += 1
+    while len(tree) < count:
+        time.sleep(0.005)
+    if depth == 1:
+        # if this is the last search, we find the move with the largest score and return a tuple
+        max = tree[0]
+        for item in tree:
+            if item[0] > max[0]:
+                max = item
+        return max        
+    else:
+        # if not we would find the best enemy move and best counter move
+        # combining these and adding up the total score to get the move with the highest score
+        score_board = []
+        for item in tree:
+            context = commit_move(item[1], item[2], current)
+            enemy_tree = []
+            count = 0
+            for i in range(0,15):
+                for j in range(0,15):
+                    if context[i][j] != 0:
+                        continue
+                    is_upper_bound = j == 0
+                    is_lower_bound = j == 14
+                    is_left_bound = i == 0
+                    is_right_bound = i == 14
+                    # Check if there's a non empty neighbor and compute score if so
+                    if not is_upper_bound:
+                        if context[i][j-1] != 0:    
+                            thread = Thread(target=compute_opening.compute_score_rev,args=(context.copy(),i,j,enemy_tree))
+                            thread.start()
+                            count += 1
+                        if not is_left_bound:
+                            if context[i-1][j-1] != 0 or current[i-1][j] != 0: 
+                                thread = Thread(target=compute_opening.compute_score_rev,args=(context.copy(),i,j,enemy_tree))
+                                thread.start()
+                                count += 1
+                        if not is_right_bound:
+                            if context[i+1][j-1] or current[i+1][j] != 0: 
+                                thread = Thread(target=compute_opening.compute_score_rev,args=(context.copy(),i,j,enemy_tree))
+                                thread.start()
+                                count += 1
+                    if not is_lower_bound:
+                        if context[i][j+1] != 0:
+                            thread = Thread(target=compute_opening.compute_score_rev,args=(context.copy(),i,j,enemy_tree))
+                            thread.start()
+                            count += 1
+                        if not is_left_bound and context[i-1][j+1]:
+                            thread = Thread(target=compute_opening.compute_score_rev,args=(context.copy(),i,j,enemy_tree))
+                            thread.start()
+                            count += 1
+                        if not is_right_bound and context[i+1][j+1]:
+                            thread = Thread(target=compute_opening.compute_score_rev,args=(context.copy(),i,j,enemy_tree))
+                            thread.start()
+                            count += 1
+                        if not is_left_bound and context[i-1][j+1]:
+                            thread = Thread(target=compute_opening.compute_score_rev,args=(context,i,j,enemy_tree))
+                            thread.start()
+                            count += 1
+                        if not is_right_bound and context[i+1][j+1]:
+                            thread = Thread(target=compute_opening.compute_score_rev,args=(context,i,j,enemy_tree))
+                            thread.start()
+                            count += 1
+            # find the best enemy move
+            while len(enemy_tree)<count:
+                time.sleep(0.005)
+            max = enemy_tree[0]
+            for some in enemy_tree:
+                if item[0] > max[0]:
+                    max = some   
+            new_depth = depth - 2
+            context = commit_move_rev(max[1], max[2], context)
+            # search recursively
+            next_best_move = search(new_depth, context)
+            uncommit_move(max[1], max[2], context)
+            # find the overall best move
+            score_board.append([item[0]-max[0]+next_best_move[0],item[1],item[2]]) 
+            uncommit_move(item[1], item[2], current)
+        max = score_board[0]
+        #  find the overall best move
+        for item in score_board:
+            if item[0] > max[0]:
+                max = item  
+        return max
+             
+def commit_move(x, y, current):
+    current[x][y] = 1
+    return current       
+
+def commit_move_rev(x, y, current):
+    current[x][y] = 2
+    return current      
+
+def uncommit_move(x, y, current):
+    current[x][y] = 0
+    return current      
+
